542b11c1bf0c249e416bbe8510b5601aca67c0ca
Jason Goodney <Jason.Goodney@gmail.com>
2018-09-12 08:09:07 -0600

    Initial commit

---
+++ Rover/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rover
+//
+//  Created by Jason Goodney on 9/12/18.
+//  Copyright © 2018 Jason Goodney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
