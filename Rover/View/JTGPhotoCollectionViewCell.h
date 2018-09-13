fe559c401e97ae6284d816bc7a492f1d5a6ad394
Jason Goodney <Jason.Goodney@gmail.com>
2018-09-13 12:05:20 -0600

    Creates SolDescriptions of Rover and populates SolsTableViewController

---
+++ Rover/View/JTGPhotoCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  JTGPhotoCollectionViewCell.h
+//  Rover
+//
+//  Created by Jason Goodney on 9/13/18.
+//  Copyright © 2018 Jason Goodney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTGPhotoCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Rover/View/JTGPhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  JTGPhotoCollectionViewCell.m
+//  Rover
+//
+//  Created by Jason Goodney on 9/13/18.
+//  Copyright © 2018 Jason Goodney. All rights reserved.
+//
+
+#import "JTGPhotoCollectionViewCell.h"
+
+@implementation JTGPhotoCollectionViewCell
+
+@end
